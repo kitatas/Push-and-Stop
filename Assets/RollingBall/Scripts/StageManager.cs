d92b3f33a9cc7d453ee59bae382db9618cc01c50
ktzwnoy <315kitatas10703@gmail.com>
2019-09-06 00:48:03 +0900

    Add ClearAnimation

--- Assets/RollingBall/Scripts/StageManager.cs
+++ Assets/RollingBall/Scripts/StageManager.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using CharTween;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using Zenject;
@@ -9,7 +10,7 @@ public class StageManager : MonoBehaviour
     [Inject] private readonly StageDataTable _stageDataTable = default;
     [Inject] private readonly TextMeshProUGUI _clearText = default;
 
-    [SerializeField] private Transform _goal = null;
+    [SerializeField] private Transform goal = null;
 
     public Vector2 goalPosition { get; private set; }
 
@@ -17,15 +18,44 @@ public class StageManager : MonoBehaviour
     {
         var index = _stageDataTable.stageIndex;
         _diContainer.InstantiatePrefab(_stageDataTable.stageData[index]);
-        _goal.position = _stageDataTable.goalPosition[index];
+        goal.position = _stageDataTable.goalPosition[index];
 
         goalPosition = GameObject.FindGameObjectWithTag("Goal").transform.position;
     }
 
     public void DisplayClearText()
     {
-        _clearText.transform
-            .DOLocalMoveY(0f, 1f)
-            .SetEase(Ease.OutBounce);
+        TweenClearText();
+    }
+
+    private void TweenClearText()
+    {
+        var tweener = _clearText.GetCharTweener();
+        var characterCount = tweener.CharacterCount;
+        var sequence = DOTween.Sequence();
+
+        for (var i = 0; i < characterCount; ++i)
+        {
+            var t = i / (float) characterCount;
+            var timeOffset = Mathf.Lerp(0, 1, t);
+
+            var charSequence = DOTween.Sequence();
+            charSequence
+                .Append(tweener
+                    .DOLocalMoveY(i, 0.5f, 0.5f)
+                    .SetEase(Ease.InOutCubic))
+                .Join(tweener
+                    .DOFade(i, 0, 0.5f)
+                    .From())
+                .Join(tweener
+                    .DOScale(i, 0, 0.5f)
+                    .From()
+                    .SetEase(Ease.OutBack, 5))
+                .Append(tweener
+                    .DOLocalMoveY(i, 0, 0.5f)
+                    .SetEase(Ease.OutBounce));
+
+            sequence.Insert(timeOffset, charSequence);
+        }
     }
 }
