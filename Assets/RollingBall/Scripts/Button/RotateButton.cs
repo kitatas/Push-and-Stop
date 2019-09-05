62ff7078e8518aacdec0091d44d73b6260132236
ktzwnoy <315kitatas10703@gmail.com>
2019-09-06 00:47:02 +0900

    Update Color

--- Assets/RollingBall/Scripts/Button/RotateButton.cs
+++ Assets/RollingBall/Scripts/Button/RotateButton.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class RotateButton : BaseButton
 {
     private Image _image;
+    private readonly Color _activateColor = new Color(0.2f, 0.7f, 0.7f);
+    private readonly Color _deactivateColor = new Color(0.1f, 0.4f, 0.5f);
 
     private readonly Subject<Unit> _subject = new Subject<Unit>();
     public IObservable<Unit> OnPushed() => _subject;
@@ -27,6 +29,6 @@ public class RotateButton : BaseButton
     public void ActivateButton(bool value)
     {
         button.enabled = value;
-        _image.color = value ? Color.white : Color.gray;
+        _image.color = value ? _activateColor : _deactivateColor;
     }
 }
