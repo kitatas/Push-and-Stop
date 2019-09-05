dc3dcf8aac30de7a3e742ee15034546a205f1893
ktzwnoy <315kitatas10703@gmail.com>
2019-09-05 21:16:43 +0900

    Refactor Block

--- Assets/RollingBall/Scripts/Block/Block.cs
+++ Assets/RollingBall/Scripts/Block/Block.cs
@@ -4,7 +4,12 @@ using Zenject;
 public class Block : MonoBehaviour, IHittable
 {
     [Inject] private readonly PlayerController _playerController = default;
-    
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
     public void Hit(Vector3 moveDirection)
     {
         _playerController.ActivatePlayerButton();
