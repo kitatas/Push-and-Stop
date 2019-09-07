638c9855403dee9288033d4738ea3418bcc50d32
ktzwnoy <315kitatas10703@gmail.com>
2019-09-08 08:45:48 +0900

    Add Information

---
+++ Assets/RollingBall/Scripts/Button/InfoButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class InfoButton : BaseButton
+{
+    [SerializeField] private GameObject infoObj = null;
+    [SerializeField] private bool isActive = default;
+
+    protected override void OnPush()
+    {
+        base.OnPush();
+
+        infoObj.SetActive(isActive);
+    }
+}
