254521c361fdb4e1ff2c4d2d3a7d43a63e837f22
kita <315kitatas10703@gmail.com>
2019-09-13 19:46:06 +0900

    Add Option

---
+++ Assets/RollingBall/Scripts/Option/LoadOption.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOption : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("Option", LoadSceneMode.Additive);
+    }
+}
