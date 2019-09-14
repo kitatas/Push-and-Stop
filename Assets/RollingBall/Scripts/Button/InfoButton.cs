ce9fb28e90bd521a0a08352f6af5661acc6266f8
ktzwnoy <315kitatas10703@gmail.com>
2019-09-14 22:18:30 +0900

    Update TitleScene

--- Assets/RollingBall/Scripts/Button/InfoButton.cs
+++ Assets/RollingBall/Scripts/Button/InfoButton.cs
@@ -1,14 +1,27 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using Zenject;
 
 public class InfoButton : BaseButton
 {
-    [SerializeField] private GameObject infoObj = null;
+    [Inject] private readonly RectTransform _infoObj = default;
     [SerializeField] private bool isActive = default;
+    private float _originHeight;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _originHeight = _infoObj.localPosition.y;
+    }
 
     protected override void OnPush()
     {
         base.OnPush();
 
-        infoObj.SetActive(isActive);
+        var h = isActive ? 0 : _originHeight;
+
+        _infoObj
+            .DOLocalMoveY(h, ConstantList.uiAnimationTime / 2f);
     }
 }
