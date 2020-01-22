99a7e79cc7f4b86ff1284cf6e7e77314cf39e8e4
ktzwnoy <315kitatas10703@gmail.com>
2020-01-22 21:33:39 +0900

    Refactor PopUpButton

--- Assets/RollingBall/Scripts/Button/PopUpButton.cs
+++ Assets/RollingBall/Scripts/Button/PopUpButton.cs
@@ -8,10 +8,23 @@ public sealed class PopUpButton : BaseButton
     [SerializeField] private bool isActivate = default;
 
     private const float animationTime = 0.25f;
-    private const Ease easeOpen = Ease.OutBack;
-    private const Ease easeClose = Ease.InBack;
-    private readonly Vector3 _v111 = Vector3.one;
-    private readonly Vector3 _v888 = new Vector3(0.8f, 0.8f, 0.8f);
+
+    private struct EaseType
+    {
+        public readonly Ease _ease;
+        public readonly float _alpha;
+        public readonly Vector3 _scale;
+
+        public EaseType(Ease ease, float alpha, Vector3 scale)
+        {
+            _ease = ease;
+            _alpha = alpha;
+            _scale = scale;
+        }
+    }
+
+    private readonly EaseType _open = new EaseType(Ease.OutBack, 1f, Vector3.one);
+    private readonly EaseType _close = new EaseType(Ease.InBack, 0f, Vector3.one * 0.8f);
 
     protected override void OnPush()
     {
@@ -25,31 +38,26 @@ public sealed class PopUpButton : BaseButton
         if (isActivate)
         {
             canvasGroup.blocksRaycasts = true;
-            AlphaAnimation(easeOpen, 1f);
-            ScaleAnimation(easeOpen, _v111);
+            TweenWindow(_open);
         }
         else
         {
             canvasGroup.blocksRaycasts = false;
-            AlphaAnimation(easeClose, 0f);
-            ScaleAnimation(easeClose, _v888);
+            TweenWindow(_close);
         }
     }
 
-    private void AlphaAnimation(Ease ease, float endValue)
+    private void TweenWindow(EaseType easeType)
     {
-        DOTween.To(
-                () => canvasGroup.alpha,
-                alpha => canvasGroup.alpha = alpha,
-                endValue,
-                animationTime)
-            .SetEase(ease);
-    }
-
-    private void ScaleAnimation(Ease ease, Vector3 scale)
-    {
-        window
-            .DOScale(scale, animationTime)
-            .SetEase(ease);
+        DOTween.Sequence()
+            .Append(DOTween
+                .To(() => canvasGroup.alpha,
+                    alpha => canvasGroup.alpha = alpha,
+                    easeType._alpha,
+                    animationTime)
+                .SetEase(easeType._ease))
+            .Join(window
+                .DOScale(easeType._scale, animationTime)
+                .SetEase(easeType._ease));
     }
 }
