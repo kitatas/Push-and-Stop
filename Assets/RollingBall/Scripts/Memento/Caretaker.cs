39668d5b6a02e382498fd6a103b5b6d79169a563
ktzwnoy <315kitatas10703@gmail.com>
2020-02-29 08:17:09 +0900

    Add Memento

---
+++ Assets/RollingBall/Scripts/Memento/Caretaker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Caretaker : MonoBehaviour
+{
+    private List<IMoveObject> _moveObjects;
+    private Stack<List<Memento>> _mementoStack;
+
+    public void Initialize()
+    {
+        _mementoStack = new Stack<List<Memento>>();
+
+        _moveObjects = new List<IMoveObject>();
+        foreach (var component in FindObjectsOfType<Component>())
+        {
+            if (component is IMoveObject moveObject)
+            {
+                _moveObjects.Add(moveObject);
+            }
+        }
+    }
+
+    public bool IsMementoStackEmpty() => _mementoStack.Count == 0;
+
+    public void PushMementoStack()
+    {
+        var mementos = new List<Memento>();
+        foreach (var moveObject in _moveObjects)
+        {
+            var memento = new Memento(moveObject.GetPosition());
+            mementos.Add(memento);
+        }
+
+        _mementoStack.Push(mementos);
+    }
+
+    public void PopMementoStack()
+    {
+        var mementoList = _mementoStack.Peek();
+        for (int i = 0; i < _moveObjects.Count; i++)
+        {
+            _moveObjects[i].SetPosition(mementoList[i].GetPosition());
+        }
+
+        _mementoStack.Pop();
+    }
+}
---
+++ Assets/RollingBall/Scripts/Memento/Memento.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public sealed class Memento
+{
+    private readonly Vector3 _position;
+
+    public Memento(Vector3 position)
+    {
+        _position = position;
+    }
+
+    public Vector3 GetPosition() => _position;
+}
