40af001770098f0e01bba47d3423b8b4c2e1b8a6
ktzwnoy <315kitatas10703@gmail.com>
2020-02-29 08:17:38 +0900

    Add UndoButton

---
+++ Assets/RollingBall/Scripts/Button/UndoButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Zenject;
+
+public sealed class UndoButton : BaseButton
+{
+    private IMoveCountUpdatable _moveCountUpdatable;
+    private Caretaker _caretaker;
+
+    [Inject]
+    private void Construct(IMoveCountUpdatable moveCountUpdatable, Caretaker caretaker)
+    {
+        _moveCountUpdatable = moveCountUpdatable;
+        _caretaker = caretaker;
+    }
+
+    protected override void OnPush()
+    {
+        if (_caretaker.IsMementoStackEmpty())
+        {
+            return;
+        }
+
+        base.OnPush();
+
+        _moveCountUpdatable.UpdateMoveCount(UpdateType.Decrease);
+
+        _caretaker.PopMementoStack();
+    }
+
+    public void InteractButton(bool value)
+    {
+        button.interactable = false;
+    }
+}
