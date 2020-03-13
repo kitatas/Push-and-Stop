e4f9488e752c1ab3d2d78d5807f9905fc7f8051f
ktzwnoy <315kitatas10703@gmail.com>
2020-03-13 21:32:12 +0900

    Update MinMoveCountView

--- Assets/RollingBall/Scripts/StageData/MinMoveCountView.cs
+++ Assets/RollingBall/Scripts/StageData/MinMoveCountView.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public sealed class MinMoveCountView : MonoBehaviour
 {
+    public int minCount { get; private set; }
+
     public void Display(int minCount)
     {
+        this.minCount = minCount;
         GetComponent<TextMeshProUGUI>().text = $"{minCount}";
     }
 }
