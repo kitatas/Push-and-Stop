2e8409c06e6d6cbeba748172b24e3b3e7b7bbb4e
ktzwnoy <315kitatas10703@gmail.com>
2020-03-24 20:11:37 +0900

    Refactor RankLoader

--- Assets/RollingBall/Scripts/StageData/RankLoader.cs
+++ Assets/RollingBall/Scripts/StageData/RankLoader.cs
@@ -3,19 +3,20 @@ using UnityEngine.UI;
 
 public sealed class RankLoader : MonoBehaviour
 {
-    [SerializeField, Range(0, 9)] private int stageNumber = 0;
+    private int _stageNumber;
     [SerializeField] private Image[] rankImages = null;
     private readonly Color _fillColor = new Color(255f / 255f, 180f / 255f, 0f / 255f);
     private readonly Color _emptyColor = new Color(71f / 255f, 71f / 255f, 68f / 255f);
 
     private void Awake()
     {
+        _stageNumber = GetComponent<LoadButton>().stageNumber;
         LoadRank();
     }
 
     public void LoadRank()
     {
-        var key = ConstantList.GetKeyName(stageNumber);
+        var key = ConstantList.GetKeyName(_stageNumber);
         var rank = ES3.Load(key, 0);
 
         for (int i = 0; i < rankImages.Length; i++)
