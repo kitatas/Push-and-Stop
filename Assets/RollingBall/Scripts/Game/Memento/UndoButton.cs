bddf47563d2c363dc90073503973ea8df1b9c0d1
kitatas <315kitatas10703@gmail.com>
2021-01-20 00:23:23 +0900

    fix: UndoButton修正

---
+++ Assets/RollingBall/Scripts/Game/Memento/UndoButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RollingBall.Common.Button;
+using UniRx;
+using UnityEngine;
+
+namespace RollingBall.Game.Memento
+{
+    /// <summary>
+    /// 一手前に戻るボタン
+    /// </summary>
+    [RequireComponent(typeof(ButtonActivator))]
+    public sealed class UndoButton : MonoBehaviour
+    {
+        private readonly Subject<Action> _subject = new Subject<Action>();
+        public IObservable<Action> onPush => _subject;
+
+        public ButtonActivator buttonActivator { get; private set; }
+
+        private void Awake()
+        {
+            buttonActivator = GetComponent<ButtonActivator>();
+        }
+
+        private void Start()
+        {
+            GetComponent<UnityEngine.UI.Button>()
+                .OnClickAsObservable()
+                .Subscribe(_ => _subject.OnNext(() => buttonActivator.SetInteractable(false)))
+                .AddTo(this);
+        }
+    }
+}
