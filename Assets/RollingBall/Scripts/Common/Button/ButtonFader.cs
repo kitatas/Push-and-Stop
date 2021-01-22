d2187f68fbc433e11b4e082f46518a14a381e01a
kitatas <315kitatas10703@gmail.com>
2021-01-22 19:03:05 +0900

    add: ボタンのFade機能

---
+++ Assets/RollingBall/Scripts/Common/Button/ButtonFader.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RollingBall.Common.Button
+{
+    [RequireComponent(typeof(Image))]
+    [RequireComponent(typeof(ButtonActivator))]
+    public sealed class ButtonFader : MonoBehaviour
+    {
+        private Image _image;
+        private ButtonActivator _buttonActivator;
+
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+            _buttonActivator = GetComponent<ButtonActivator>();
+        }
+
+        private void Start()
+        {
+            _buttonActivator.SetInteractable(false);
+        }
+
+        public async UniTask ShowAsync(CancellationToken token)
+        {
+            _buttonActivator.SetEnabled(false);
+            _buttonActivator.SetInteractable(true);
+
+            await _image
+                .DOFade(1.0f, Const.UI_ANIMATION_TIME)
+                .WithCancellation(token);
+
+            _buttonActivator.SetEnabled(true);
+        }
+    }
+}
