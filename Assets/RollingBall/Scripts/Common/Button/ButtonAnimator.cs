d2a2ae2886382718d42adc427d3aaf0d0d726903
kitatas <315kitatas10703@gmail.com>
2021-02-15 20:54:02 +0900

    fix: Button有効時のみアニメーションするように

--- Assets/RollingBall/Scripts/Common/Button/ButtonAnimator.cs
+++ Assets/RollingBall/Scripts/Common/Button/ButtonAnimator.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 
 namespace RollingBall.Common.Button
 {
+    [RequireComponent(typeof(ButtonActivator))]
     [RequireComponent(typeof(UnityEngine.UI.Button))]
     public sealed class ButtonAnimator : MonoBehaviour
     {
+        private ButtonActivator _buttonActivator;
+
+        private readonly float _animationTime = 0.1f;
+
+        private void Awake()
+        {
+            _buttonActivator = GetComponent<ButtonActivator>();
+        }
+
         private void Start()
         {
             var button = GetComponent<UnityEngine.UI.Button>();
+            var target = button.image.rectTransform;
 
             button
                 .OnPointerDownAsObservable()
+                .Where(_ => _buttonActivator.IsInteractable() && _buttonActivator.IsEnabled())
                 .Subscribe(_ =>
                 {
-                    button.image.rectTransform
-                        .DOScale(Vector3.one * 0.85f, 0.1f);
+                    target
+                        .DOScale(Vector3.one * 0.85f, _animationTime);
                 })
                 .AddTo(this);
 
@@ -25,8 +37,8 @@ namespace RollingBall.Common.Button
                 .OnPointerUpAsObservable()
                 .Subscribe(_ =>
                 {
-                    button.image.rectTransform
-                        .DOScale(Vector3.one, 0.1f);
+                    target
+                        .DOScale(Vector3.one, _animationTime);
                 })
                 .AddTo(this);
         }
