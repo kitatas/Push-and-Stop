ffe8012d971554bf94682064cfd55e251b360d0a
kitatas <315kitatas10703@gmail.com>
2023-08-26 03:48:40 +0900

    fix: 外部サイトに遷移するボタン

---
+++ Assets/RollingBall/Scripts/Title/ExternalSiteButton.cs
@@ -0,0 +1,40 @@
+using System;
+using RollingBall.Common.Button;
+using UniRx;
+using UnityEngine;
+
+namespace RollingBall.Title
+{
+    [RequireComponent(typeof(ButtonActivator))]
+    [RequireComponent(typeof(ButtonSpeaker))]
+    public sealed class ExternalSiteButton : MonoBehaviour
+    {
+        private enum ExternalSiteType
+        {
+            None,
+            Credit,
+            License,
+            Policy,
+        }
+
+        [SerializeField] private ExternalSiteType type = default;
+
+        private void Start()
+        {
+            var external = type switch
+            {
+                ExternalSiteType.Credit => "credit",
+                ExternalSiteType.License => "license",
+                ExternalSiteType.Policy => "policy",
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null),
+            };
+
+            var url = $"https://kitatas.github.io/Push-and-Stop/{external}";
+
+            GetComponent<UnityEngine.UI.Button>()
+                .OnClickAsObservable()
+                .Subscribe(_ => Application.OpenURL(url))
+                .AddTo(this);
+        }
+    }
+}
